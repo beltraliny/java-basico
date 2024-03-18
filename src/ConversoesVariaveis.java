d78bfdffba3869e955abb1ccfa524df49bc232ab
aliny <beltraliny@hotmail.com>
2024-03-17 22:32:28 -0300

    Conversões de tipos primitivos

---
+++ src/ConversoesVariaveis.java
@@ -0,0 +1,22 @@
+public class ConversoesVariaveis {
+
+    public static void main(String[] args) {
+        long longNumber = 10;
+
+        // Casting é necessário quando variável final é menor que a de origem:
+        int intNumber = (int) longNumber;
+        System.out.println(intNumber); //10
+
+        // Opcional no caso contrário:
+        longNumber = intNumber;
+        System.out.println(longNumber); //10
+
+        //Outros exemplos:
+        double doubleNumber = 11.0;
+        float floatNumber = (float) doubleNumber;
+        System.out.println(floatNumber); // 11.0
+
+        int anotherIntNumber = (int) doubleNumber;
+        System.out.println(anotherIntNumber); // 11
+    }
+}
