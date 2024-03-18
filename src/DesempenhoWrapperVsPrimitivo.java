cc44069e88ab48d7a536ca8a373b0c5cf44257e1
aliny <beltraliny@hotmail.com>
2024-03-17 23:33:43 -0300

    Comparando desempendo de classe Wrapper com tipos primitivos

---
+++ src/DesempenhoWrapperVsPrimitivo.java
@@ -0,0 +1,17 @@
+public class DesempenhoWrapperVsPrimitivo {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        // long sum = 0L;
+        Long sum = 0L;
+        for (long i = 0; i< Integer.MAX_VALUE; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+
+        double totalTime = (System.currentTimeMillis() - start) / 1000d;
+        System.out.printf("Performance: %.2fs%n", totalTime);
+    }
+}
