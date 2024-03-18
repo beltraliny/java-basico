132f87b0c91e900f1378efba5ae4ffed0b78eccb
aliny <beltraliny@hotmail.com>
2024-03-17 23:28:02 -0300

    Comparando Wrappers

---
+++ src/ComparandoWrappers.java
@@ -0,0 +1,26 @@
+public class ComparandoWrappers {
+
+    public static void main(String[] args) {
+        Integer number = 127;
+        Integer number0 = 127;
+
+        // Objetos do tipo Integer com valores até 127 estão alocados na memória cash, não sendo necessária uma nova
+        // instância, o operador de igualdade funciona:
+        System.out.println(number == number0); // true
+
+
+        Integer number1 = 128;
+        Integer number2 = 128;
+
+        // A partir do valor 128 são instanciados novos objetos e como não possuem a mesma referência o operador de
+        // igualdade não funciona
+        System.out.println(number1 == number2); // false
+
+
+        Short number3 = 128;
+
+        // Forma correta de comparar Wrappers:
+        System.out.println(number1.equals(number2)); //true
+        System.out.println(number1.intValue() == number3.intValue()); //true
+    }
+}
