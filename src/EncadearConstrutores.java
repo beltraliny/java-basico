f21d29752df2128b15c6198fc9ea99fb37691248
aliny <beltraliny@hotmail.com>
2024-03-19 18:44:53 -0300

    Encadeando construtores de classe

---
+++ src/EncadearConstrutores.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public class EncadearConstrutores {
+
+    static final String STRING_DEFAULT = "Default";
+    static final int INT_DEFAULT = 0;
+
+    String stringType;
+    int intType;
+
+    EncadearConstrutores() {
+        this(STRING_DEFAULT);
+    }
+
+    EncadearConstrutores(String stringType) {
+        this(stringType, INT_DEFAULT);
+    }
+
+    EncadearConstrutores(String stringType, int intType) {
+        Objects.requireNonNull(stringType, "String required");
+        if (intType < 0) throw new IllegalArgumentException("Negative numbers are not allowed");
+
+        this.stringType = stringType;
+        this.intType = intType;
+    }
+}
