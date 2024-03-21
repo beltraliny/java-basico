8e1b10268a0c64c4f9d4a1fc2326b8f161da34e7
aliny <beltraliny@hotmail.com>
2024-03-20 22:03:16 -0300

    Regras para uma classe imutável

---
+++ src/ClassImutavel.java
@@ -0,0 +1,24 @@
+/*
+    Classes imutaveis não podem ser herdadas
+ */
+
+public final class ClassImutavel {
+    // Necessário o modificador de acesso final para a classe e todos os atributos
+    private final String stringType;
+    private final int intType;
+
+    ClassImutavel(String stringType, int intType) {
+        // Os valores não setados da declaração precisam estar no construtor para inicialização (final)
+        this.stringType = stringType;
+        this.intType = intType;
+    }
+
+    // Obviamente não possuem setters, apenas getters
+    public String getStringType() {
+        return stringType;
+    }
+
+    public int getIntType() {
+        return intType;
+    }
+}
