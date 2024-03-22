db25f467412a97425831b38cdad2b4455b74328e
aliny <beltraliny@hotmail.com>
2024-03-22 08:32:15 -0300

    Efeito do modificador final em classes e métodos

---
+++ src/com/github/beltraliny/classes/FinalEmClasseEMetodos.java
@@ -0,0 +1,9 @@
+package com.github.beltraliny.classes;
+
+// Modificador final impede que a classe seja herdada
+public final class FinalEmClasseEMetodos {
+
+    public final void example() {
+        System.out.println("Modificador final em métodos impede que ele seja sobrescrito por classes filhas.");
+    }
+}
